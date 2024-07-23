020b738e80e34936ed63d8e8e49ed9c45088217c
MrFarmerBon <[benjholmes123@gmail.com]>
2024-07-23 21:16:15 +0100

    Finished Game-play

--- script.js
+++ script.js
@@ -1,61 +1,23 @@
-// const choices = ["Rock", "Paper", "Scissors"];
 
+function compare(userChoice,compChoice){
+    if(userChoice === "Rock" && compChoice ==="Scissors"|| userChoice === "Paper" && compChoice ==="Rock"||userChoice === "Scissors" && compChoice ==="Paper"){  
+        userScore++; 
+    }
+    if(userChoice === "Rock" && compChoice ==="Paper"|| userChoice === "Paper" && compChoice ==="Scissors"||userChoice === "Scissors" && compChoice ==="Rock"){
+        compScore++  
+    }
+}
+let userScore = 0;
+let compScore = 0;
 
+let userChoice =""
+let compChoice=""
 
+function updateScore(){
+    const userScoreElem = document.getElementById("userScore").innerHTML = userScore;
+    const compScoreElem = document.getElementById("compScore").innerHTML = compScore;
+}
 
-// function getUserChoice(){
-//     var userChoice = prompt("What's your choice?");
-//     if (userChoice.toLowerCase() === "rock"){
-//         userChoice = "Rock";
-//     }
-//     if (userChoice.toLowerCase() === "paper"){
-//         userChoice = "Paper";
-//     }
-//     if (userChoice.toLowerCase() === "scissors"){
-//         userChoice = "Scissors";
-//     }
-    
-//     return userChoice;
-// }
-
-// var userScore = 0;
-// var CompScore = 0;
-// var roundNum  = 1;
-
-
-// //console.log("Comp: "+getComputerChoice())
-// //console.log("User: " +getUserChoice())
-
-// function playRound(userChoice,compChoice){
-//     console.log("-----Round "+ roundNum+"-----");
-//     console.log(userChoice+" VS "+compChoice);
-
-//     if (userChoice === compChoice){
-//         console.log("It's a Draw -" + "You both played "+userChoice);
-        
-//     }
-//     if(userChoice === "Rock" && compChoice ==="Scissors"|| userChoice === "Paper" && compChoice ==="Rock"||userChoice === "Scissors" && compChoice ==="Paper"){
-//         console.log("You Win!");    
-//         userScore++; 
-//     }
-//     if(userChoice === "Rock" && compChoice ==="Paper"|| userChoice === "Paper" && compChoice ==="Scissors"||userChoice === "Scissors" && compChoice ==="Rock"){
-//         console.log("You Lost!");   
-//         CompScore++  
-//     }
-//     roundNum++;
-//     console.log("Your Score-"+ userScore);
-//     console.log("Computer Score-"+ CompScore+"\n");
-// }
-
-
-
-
-// for (let i = 0; i < 5; i++) {
-//     playRound(getUserChoice(),getComputerChoice());
-//   }
-// console.log("---FINAL SCORES---")
-// console.log("Your Score-"+ userScore);
-// console.log("Computer Score-"+ CompScore+"\n");
 
 function getComputerChoice(){
     const max =3;
@@ -63,21 +25,24 @@ function getComputerChoice(){
     if(randValue === 0){
         compImg.style.visibility = "visible";
         compImg.src ="images/stone.png";
+        return compChoice ="Rock";
     }
     if(randValue === 1){
         compImg.style.visibility = "visible";
         compImg.src ="images/paper.png";
+        return compChoice ="Paper";
     }
     if(randValue === 2){
         compImg.style.visibility = "visible";
         compImg.src ="images/scissor.png";
+        return compChoice ="Scissors";
     }
-    return compChoice;
 }
 
 const rockButton = document.querySelector("button.rock");
 const paperButton = document.querySelector("button.paper");
 const scissorsButton = document.querySelector("button.scissors");
+
 
 let userImg = document.querySelector("img");
 userImg.style.visibility = "hidden";
@@ -88,17 +53,27 @@ compImg.style.visibility = "hidden";
 rockButton.addEventListener("click",()=>{
     userImg.style.visibility = "visible";
     userImg.src ="images/stone.png";
+    userChoice ="Rock";
     getComputerChoice()
+    compare(userChoice,getComputerChoice());
+    updateScore()
+
 })
 
 paperButton.addEventListener("click",()=>{
     userImg.style.visibility = "visible";
     userImg.src ="images/paper.png";
+    userChoice ="Paper";
     getComputerChoice()
+    compare(userChoice,getComputerChoice());
+    updateScore()
 })
 
 scissorsButton.addEventListener("click",()=>{
     userImg.style.visibility = "visible";
     userImg.src ="images/scissor.png";
+    userChoice ="Scissors";
     getComputerChoice()
+    compare(userChoice,getComputerChoice());
+    updateScore()
 })
